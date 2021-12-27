0da5565a8480a55cf6b02353454a0a0dc31c8c5d
Stefan Hoth <45467+stefanhoth@users.noreply.github.com>
2021-12-27 20:39:49 +0100

    feat: Add placeholder until video is selected

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -2,6 +2,13 @@ import React, { Component } from "react";
 
 export default class VideoDetail extends Component {
   render() {
-    return <div className="video-detail">VideoDetail</div>;
+    return (
+      <div className="video-detail ui placeholder segment">
+        <div class="ui icon header">
+          <i class="youtube icon"></i>
+          Search for a video and select one to play.
+        </div>
+      </div>
+    );
   }
 }
